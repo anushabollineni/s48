d229e8b05f34554bac51adf28e3645abc81b609d
anushabollineni <41730220+anushabollineni@users.noreply.github.com>
2018-09-28 10:44:47 +0530

    Create s48.java

---
+++ s48.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int Number, i;
+Scanner sc=new Scanner(System.in);
+		sc = new Scanner(System.in);
+		
+	
+		Number = sc.nextInt();
+		
+		for(i = 1; i <= Number; i++) {
+			if(Number%i == 0) {
+				System.out.printf(" %d  ", i);
+		
+}
+		}	
+}	
+}
